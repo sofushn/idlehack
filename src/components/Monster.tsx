7a81aee2ccbaeddf2be9cef72a0229dd352410ea
Sofus Hilfling <sofus@techchapter.com>
2024-04-12 23:09:53 +0000

    update death logic in monster

--- src/components/Monster.tsx
+++ src/components/Monster.tsx
@@ -8,39 +8,41 @@ interface monsterProp{
     speed: number
     gold: number
 
-    onDie?:(coinValue: number) => void
+    onDie?: (coinValue: number) => void
+    onBeforeDie?: () => void
 
     image: string
 }
 
 export function Monster(prop:monsterProp) {
-    const emptyFunc = (i: number) => {}
-    const onDie = prop.onDie ?? emptyFunc
+  const emptyFunc = (i: number) => {}
+  const emptyFunc2 = () => {} 
+  const onDie = prop.onDie ?? emptyFunc
+  const onBeforeDie = prop.onBeforeDie ?? emptyFunc2
+  const 
 
-    const [health, setHealth] = useState(prop.health_points);
+  const [health, setHealth] = useState(prop.health_points);
 
-    function loseHealth(playerDamage: number) {
-      // health cannot go below zero
-      if (health > 1) {
-        setHealth(health - playerDamage);
-      } else {
-        die()
-      }
-    }
-  
-    function attack() {
-      // not implemented
-    }
-
-    function die() {
-      onDie(prop.gold)
-    }
-    
-    return (
-      <div onClick={() => loseHealth(1)}>
-        <img src={prop.image} alt="Monster" />
-        <p>Health: {health}</p>
-      </div>
-    );
+  function loseHealth(damage: number) {
+    const newHealth = health - damage
+    setHealth(newHealth)
   }
+
+  function attack() {
+    // not implemented
+  }
+
+  if (health < 1) {
+    onBeforeDie()
+    onDie(prop.gold)
+    return <></>
+  }
+
+  return (
+    <div onClick={() => loseHealth(1)}>
+      <img src={prop.image} alt="Monster" />
+      <p>Health: {health}</p>
+    </div>
+  );
+}
   
