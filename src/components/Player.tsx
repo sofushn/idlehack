af882a3b668b9bba9217d6375145124de3b5e287
thejamiegc <jgc@techchapter.com>
2024-04-13 00:15:23 +0200

    added player

---
+++ src/components/Player.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+interface player {
+    maxHealth?: number
+    currentHealth?: number
+    attackPower?: number
+}
+
+function Player(prop: player) {
+    const [maxHealth, setMaxHealth] = useState(prop.maxHealth)
+    const [currentHealth, setCurrentHealth] = useState(prop.currentHealth)
+    const [attack, setAttack] = useState(prop.attackPower)
+
+    function heal(heal: number) {
+        if (maxHealth! <= currentHealth! + heal) {
+            setCurrentHealth(maxHealth);
+        } else {
+            setCurrentHealth(currentHealth!+heal)
+        }
+    }
+
+   
+
+
+    return (
+        <div onClick={() => heal(1)}>
+            <p>player health: {currentHealth}</p>
+
+        </div>
+    )
+}
+
+export default Player;
