8a3d4f43e1122fbbca736e7b73fa79edec016a14
Jakob <jakob.sheye@gmail.com>
2024-04-12 22:20:02 +0200

    followup commit

--- src/components/Monster.tsx
+++ src/components/Monster.tsx
@@ -1,28 +1,43 @@
 import { useState } from "react";
 
+
 interface monsterProp{
     health_points: number
     attack_power: number
     defense: number
     speed: number
     gold: number
+
+    image: string
 }
 
 export function Monster(prop:monsterProp) {
     const [health, setHealth] = useState(prop.health_points);
-  
-    function loseHealth() {
+    
+    // this class needs an image representation 
+
+    function loseHealth(playerDamage: number) {
+      // health cannot go below zero
       if (health > 0) {
-        setHealth(health - 1);
-      }
-      else {
-        // die
+        setHealth(health - playerDamage);
       }
     }
   
+    function attack() {
+      // not implemented
+    }
+
+    function die() {
+      // not implemented
+      if (health <= 0) {
+        // drop cash ($)
+      }
+    }
+    
     return (
-      <div onClick={loseHealth}>
-        Health: {health}
+      <div onClick={() => loseHealth(1)}>
+        <img src={prop.image} alt="Monster" />
+        <p>Health: {health}</p>
       </div>
     );
   }
